2792c039ee6e7a610a547c411ecf0e8f4aa2335b
Nettanel <nettanel1999@gmail.com>
2024-07-24 15:51:15 +0300

    First Commit

---
+++ exercise.9.7.cs/exercise.9.7.cs/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace exercise._9._7.cs
+{
+    internal class Car
+    {
+        private Color carColor;
+
+        public Car(Color color )
+        {
+            carColor = color;
+        }
+        public void DisplayColor()
+        {
+            Console.WriteLine(carColor.ToString());
+        }
+    }
+}
---
+++ exercise.9.7.cs/exercise.9.7.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using exercise._9._7.cs;
+using System;
+namespace exercise._9._7.cs
+{
+    enum Color { Blue, Red, Green }
+    class Program
+        {
+    
+
+        static public void Main(String[] args)
+        {
+
+            Car[] Cars = new Car[3];
+            Cars[0] = new Car(Color.Blue);
+            Cars[1] = new Car(Color.Green);
+            Cars[2] = new Car(Color.Red);
+
+            Cars[0].DisplayColor();
+            Cars[1].DisplayColor();
+            Cars[2].DisplayColor();
+
+        }
+    }
+
+}
